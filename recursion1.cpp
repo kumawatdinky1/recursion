52927a985b9dfa4c67d6568fe6b7af2a070b2bad
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-05-28 12:57:24 +0530

    Create recursion1.cpp

---
+++ recursion1.cpp
@@ -0,0 +1,15 @@
+//print name n times
+#include<iostream>
+using namespace std;
+void func( i, n )
+{
+   if(i>n) return;
+ print(“Raj”);
+   func( i+1,N );
+}
+main()
+{
+  int n;
+  input(n);
+  func(1,n);
+}
